de68549e39f91df8867048fb0a2920b8539ee785
aksldkel <44428365+aksldkel@users.noreply.github.com>
2018-11-12 21:24:15 +0900

    Revert "auction add"
    This reverts commit 6ba10b6b14adf99cbcd244c21a643d3f28a52591.

--- Newbieract/urls.py
+++ Newbieract/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('main.urls', namespace='main')),
-    url(r'^auction/', include('auction.urls', namespace='auction')),
 
 ]
 #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
