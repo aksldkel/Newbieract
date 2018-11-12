8310469c02e1f5d6bc6c7bbad0c1e14abe09b643
yjjeon6621@gmail.com <yjjeon6621@gmail.com>
2018-11-12 21:07:39 +0900

    login

--- Newbieract/urls.py
+++ Newbieract/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('main.urls', namespace='main')),
+    url(r'', include('login.urls', namespace='main')),
 
 ]
 #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
